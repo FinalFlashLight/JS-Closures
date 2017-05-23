53addddf2eb98a1967188743aabf555049d97b0e
FinalFlashLight <ryan.godwin524@outlook.com>
2017-05-22 22:27:08 -0500

    finished all but black dia and timeout

--- closures.js
+++ closures.js
@@ -15,10 +15,10 @@ closure over the name variable. Invoke outer saving the return value into
 another variable called 'inner'. */
 
 // Code Here
-
+var inner = outer();
 //Once you do that, invoke inner.
-
 //Code Here
+inner();
 
 
 
@@ -47,8 +47,8 @@ Create a callJake function that when invoked with '435-555-9248' returns 'Callin
 in your console. */
 
   //Code Here
-
-
+  var callJake = callFriend('Jake');
+  callJake(435-555-9248);
 
 
 
@@ -65,13 +65,20 @@ in your console. */
 properly. */
 
 //Code Here
-
+function makeCounter() {
+  var i = 0;
+  function count() {
+    i = i + 1;
+    return i; //must be returned or it wont pass tests
+  };
+  return count;
+}
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+   var count = makeCounter();
+   count(); // 1
+   count(); // 2
+   count(); // 3
+   count(); // 4
 
 
 
@@ -91,26 +98,34 @@ properly. */
 up/down counter. The first function is called inc, this function is responsible
 for incrementing the value once. The second function is called dec, this
 function is responsible for decrementing the value by one. You will need to use
-the module pattern to achieve this. 
-Information on the module pattern available here: 
+the module pattern to achieve this.
+Information on the module pattern available here:
 http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
 function counterFactory(value) {
-
+  var val = value
   // Code here.
 
-
   return {
+    inc: function(){
+      ++val;
+      return val; //you actually HAVE to return these or it wont pass
+    },
+    dec:function(){
+      --val;
+      return val;
+    },
+
   }
 }
 
 
-counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+var counter = counterFactory(10);
+ counter.inc() // 11
+ counter.inc() // 12
+ counter.inc() // 13
+ counter.dec() // 12
 
 
 
@@ -134,10 +149,12 @@ function motivation(firstname, lastname) {
   var welcomeText = 'You\'re doing awesome, keep it up ';
 
   // code message function here.
-
+  function message(){
+    return welcomeText + firstname + ' ' + lastname + '.';
+  }
 
   //Uncommment this to return the value of your invoked message function
-  //return message();
+  return message();
 
 }
 
@@ -175,10 +192,12 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
+    publicMethod: privateMethod,
     // Code here.
   };
 
 })();
+module.publicMethod();
 
 
 
@@ -195,12 +214,16 @@ var secondLevelFriends = ["Anne", "Harry", "Quinton"];
 var allUsers = ["Tom", "Dick", "Harry", "Anne", "Quinton", "Katie", "Mary"];
 
 function findPotentialFriends(existingFriends) {
+  var friends = existingFriends;
 
+  return function(friend){
+    return friends.indexOf(friend) < 0;
+  }
 }
 
 var isNotAFriend = findPotentialFriends( friends );
-// isNotAFriend(allUsers[0]); // false
-// isNotAFriend(secondLevelFriends[2]); // true
+ isNotAFriend(allUsers[0]); // false
+ isNotAFriend(secondLevelFriends[2]); // true
 
 
 /******************************************************************************\
@@ -237,8 +260,10 @@ to 5. What we need to do is console.log(i) so that it logs like so:
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
     setTimeout(function() {
-    	console.log(i)
-	}, i * 1000)
+    	console.log(i);
+	}, i * 1000);
+
   }
 }
+
 timeOutCounter();

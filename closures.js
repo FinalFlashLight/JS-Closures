4fcb8b4a875cfeddc6596e32bd25449f116800ed
FinalFlashLight <ryan.godwin524@outlook.com>
2017-05-24 12:09:53 -0500

    finished except black dia

--- closures.js
+++ closures.js
@@ -259,9 +259,10 @@ to 5. What we need to do is console.log(i) so that it logs like so:
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    (function(i){
     setTimeout(function() {
     	console.log(i);
-	}, i * 1000);
+	}, i * 1000);})(i);
 
   }
 }
